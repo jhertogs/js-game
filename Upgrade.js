d0e07920b454b47529887b5f462b9fc4f1dc19df
Josse Hertogs <jossehertogs@gmail.com>
2024-09-20 16:56:26 +0200

    button inside canvas kind of works now

--- Upgrade.js
+++ Upgrade.js
@@ -3,11 +3,31 @@ export class Upgrade{
     constructor(ctx, canvas){
         this.ctx = ctx
         this.canvas = canvas
+        this.canvasLeft = canvas.offsetLeft
+        this.canvasTop = canvas.offsetTop
+        this.enoughPts = false
     }
 
     upgradePopUp(points){
+
+        this.canvas.addEventListener('click', (e) => {
+
+            let x = e.pageX - this.canvasLeft
+            let y = e.pageY - this.canvasTop
+            //this.ctx.rect(190, 40, 50, 30)
+            //x y width height
+
+            //y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width
+            // y y height x x widht
+
+            if (y > 40 && y < 40 + 30 && x > 190 && x < 190 + 50 && this.enoughPts == true) {
+                alert('clicked an element');
+            }
+        })
+
         
-        console.log("upgrades", points);
+        
+        //console.log("upgrades", points);
         
         this.ctx.beginPath()
         this.ctx.font="16px Verdana";
@@ -15,7 +35,8 @@ export class Upgrade{
         this.ctx.fillText("Score: " + points,190,20);
         this.ctx.closePath();
         
-        if(points > 50){
+        if(points > 5){
+            this.enoughPts = true
             
             this.ctx.beginPath()
             this.ctx.rect(190, 40, 50, 30)
